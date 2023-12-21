803595f86219ba4e4154260a52e7c8180ebd44ee
Mattéo Donatelli <matteo.donatelli1@gmail.com>
2023-12-21 12:11:49 +0100

    Adding card

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import './assets/style.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='card'>
+        <img className='cardImg' src="https://tse4.mm.bing.net/th?id=OIP.tvaMwK3QuFxhTYg4PSNNVAHaHa&pid=Api" alt="Avatar" />
+        <div className='cardInfos'>
+          <h1 className='cardTitle'>Avatar</h1>
+          <h2 className='cardDesc'>Bonjour tout le monde !</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
